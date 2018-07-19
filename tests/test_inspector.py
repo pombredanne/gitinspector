122f30b5c2811da54fb86270e55fddd224508add
David Renault <renault@tamriel>
2018-07-19 23:19:18 +0200

    Properly test the behavior on a fake repository

--- tests/test_inspector.py
+++ tests/test_inspector.py
@@ -22,7 +22,7 @@ from gitinspector.gitinspector import Runner, __get_validated_git_repos__
 import gitinspector.extensions as extensions
 
 # Test gitinspector over a git repository present in the resources/
-# dir
+# dir, count the changes and the blames and check the metrics. 
 class RepositoryTest(unittest.TestCase):
     def test(self):
         r = Runner()
@@ -31,4 +31,22 @@ class RepositoryTest(unittest.TestCase):
         zip_ref.close()
         repos = __get_validated_git_repos__(["build/tests/repository"])
         extensions.define("c,txt")
-        r.process(repos)
+        r.include_metrics = True
+        r.process(repos, silent = True)
+        # Chack the repositories
+        self.assertEqual(len(r.repos), 1)
+        self.assertEqual(r.repos[0].name, "repository")
+        self.assertTrue(r.repos[0].location.endswith("build/tests/repository"))
+        # Check the commits
+        self.assertEqual(len(r.changes.commits), 2)
+        self.assertEqual(r.changes.commits[0].author, "Abraham Lincoln")
+        self.assertEqual(r.changes.commits[1].author, "Andrew Johnson")
+        # Check the blames
+        self.assertEqual(len(r.blames.blames.keys()), 2)
+        blame_keys = list(r.blames.blames.keys())
+        self.assertEqual(blame_keys[0], ('Abraham Lincoln', 'README.txt'))
+        self.assertEqual(blame_keys[1], ('Andrew Johnson', 'file.c'))
+        self.assertEqual(r.blames.blames[blame_keys[0]].rows, 1) # README.txt is 1 line long
+        self.assertEqual(r.blames.blames[blame_keys[1]].rows, 6) # main.c     is 6 lines long
+        # Check the metrics
+        self.assertEqual(r.metrics.eloc, {}) # Both files are too short, no metrics to report
