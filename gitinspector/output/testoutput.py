076c2204a5f5f771280cc8b7d1c77b056b749498
David Renault <renaultd@free.fr>
2018-08-14 19:38:11 +0200

    Add a d3js Outputable class that mimicks the ChangesOutput
    This class uses d3js for drawing (instead of JQuery and the dated
    Flot JS library). It also uses a system of templates (via the basic
    python string.Template) to put the code directly in an HTML page.

---
+++ gitinspector/output/testoutput.py
@@ -0,0 +1,51 @@
+from .outputable import Outputable
+import string
+
+
+class TestOutput(Outputable):
+    output_order = 110
+
+    def __init__(self, runner):
+        Outputable.__init__(self)
+        self.changes = runner.changes
+        self.display = True
+        self.out = runner.out
+
+    def output_html(self):
+        authorinfo_dict = self.changes.get_authorinfo_list()
+        author_list = list(authorinfo_dict.keys())
+        author_list.sort(key=lambda x: authorinfo_dict[x].insertions + authorinfo_dict[x].deletions, reverse=True)
+        data_array = []
+
+        # Compute total changes
+        total_changes = 0.0
+        for i in authorinfo_dict:
+            total_changes += authorinfo_dict.get(i).insertions
+            total_changes += authorinfo_dict.get(i).deletions
+
+        for entry in author_list:
+            authorinfo = authorinfo_dict.get(entry)
+            percentage = 0 if total_changes == 0 else \
+                (authorinfo.insertions + authorinfo.deletions) / total_changes * 100
+            data_array.append({
+                "name":entry,
+                "commits" : authorinfo.commits,
+                "insertions" : authorinfo.insertions,
+                "deletions" : authorinfo.deletions,
+                "changes" : "{0:.2f}".format(percentage),
+                })
+
+        with open("gitinspector/html/test_output.html", 'r') as infile:
+            src = string.Template( infile.read() )
+            self.out.write(src.substitute(
+                data=str(data_array),
+            ))
+
+    def output_text(self):
+        pass
+
+    def output_json(self):
+        pass
+
+    def output_xml(self):
+        pass
