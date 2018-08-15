0268c6e7b35c03ffac9e369eb0caea7732417391
David Renault <renaultd@free.fr>
2018-08-16 00:54:38 +0200

    Create a second TestOutput class mimicking BlameOutput
    All files and code are currently in temporary locations. This is an
    intermediary commit, and it remains to check if the code can be
    refactored more properly. Remarks :
    
    - the JS for both outputs are now isolated, no variable from the first
    can go to the second.
    
    - the colors are problematic, because they may be different between the
    outputs. A solution would be to make the python compute the colors, and
    then send them to the outputs.
    
    - some tests are not passing for this commit

--- gitinspector/output/testoutput.py
+++ gitinspector/output/testoutput.py
@@ -2,6 +2,7 @@ from .outputable import Outputable
 from .. import gravatar
 import string
 
+from ..blame import Blame
 
 class TestOutput(Outputable):
     output_order = 110
@@ -41,7 +42,52 @@ class TestOutput(Outputable):
         with open("gitinspector/html/test_output.html", 'r') as infile:
             src = string.Template( infile.read() )
             self.out.write(src.substitute(
-                data=str(data_array),
+                changes_data=str(data_array),
+            ))
+
+    def output_text(self):
+        pass
+
+    def output_json(self):
+        pass
+
+    def output_xml(self):
+        pass
+
+
+class TestOutput2(Outputable):
+    output_order = 120
+
+    def __init__(self, runner):
+        Outputable.__init__(self)
+        self.changes = runner.changes
+        self.blames = runner.blames
+        self.display = True
+        self.out = runner.out
+
+    def output_html(self):
+        blames_list = list(self.blames.get_summed_blames().items())
+        blames_list.sort(key=lambda x: x[1].rows, reverse=True)
+        total_blames = 0
+        for i in blames_list:
+            total_blames += i[1].rows
+
+        data_array = []
+
+        for entry in blames_list:
+            data_array.append({
+                "avatar": "<img src=\"{0}\"/>".format(gravatar.get_url(self.changes.get_latest_email_by_author(entry[0]))),
+                "name":  entry[0],
+                "rows": entry[1].rows,
+                "stability": "{0:.1f}".format(Blame.get_stability(entry[0], entry[1].rows, self.changes)),
+                "age": "{0:.1f}".format(float(entry[1].skew) / entry[1].rows),
+                "comments": "{0:.2f}".format(100.0 * entry[1].comments / entry[1].rows),
+            })
+
+        with open("gitinspector/html/test_output2.html", 'r') as infile:
+            src = string.Template( infile.read() )
+            self.out.write(src.substitute(
+                remains_data=str(data_array),
             ))
 
     def output_text(self):
