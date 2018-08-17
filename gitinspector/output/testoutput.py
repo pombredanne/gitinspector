84bbbaad8fff85d956c376697d04950fdfcbe7b1
David Renault <renaultd@free.fr>
2018-08-17 19:29:02 +0200

    Add a very preliminary TestOutput mimicking TimelineOutput

--- gitinspector/output/testoutput.py
+++ gitinspector/output/testoutput.py
@@ -1,5 +1,5 @@
 from .outputable import Outputable
-from .. import gravatar
+from .. import gravatar, timeline
 import string
 
 from ..blame import Blame
@@ -11,11 +11,31 @@ class TestOutput(Outputable):
     def __init__(self, runner):
         Outputable.__init__(self)
         self.changes = runner.changes
-        self.blames = runner.blames
+        self.weeks = runner.config.weeks
         self.display = True
         self.out = runner.out
 
     def output_html(self):
+        data = timeline.TimelineData(self.changes, self.weeks)
+        authors = [author[0] for author in data.get_authors()]
+        entries = { a: {} for a in authors }
+        for k, v in data.entries.items():
+            author = k[0]
+            period = k[1]
+            entries[author][period] = [v.insertions, v.deletions, v.commits]
+
+        timeline_dict = {
+            "periods": data.get_periods(),
+            "authors": authors,
+            "changes": data.total_changes_by_period,
+            "entries": entries,
+        }
+
+        with open("gitinspector/templates/test_output.html", 'r') as infile:
+            src = string.Template( infile.read() )
+            self.out.write(src.substitute(
+                timeline=str(timeline_dict)
+            ))
         pass
 
     def output_text(self):
